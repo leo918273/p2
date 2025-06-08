aea3d3702e04fffb5457c8d50fd8df2812f5dfb4
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 19:31:37 -0300

    Criar classe ServicoPedido com dependências/ServicoDePedidos.cs

---
+++ ServicoDePedidos.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2
+{
+  public class ServicoPedido
+  {
+    private readonly RepositorioPedido _repositorio;
+    private readonly ILog _logger;
+    private readonly List<IDescStrategy> _estrategias;
+
+    public ServicoPedido(RepositorioPedido repositorio, ILog logger, List<IDescStrategy> estrategias)
+    {
+       _repositorio = repositorio;
+      _logger = logger;
+      _estrategias = estrategias;
+    }
+  }
+}
