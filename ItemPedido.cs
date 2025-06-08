998ee937b4146f0c459b7654e6bd332bd4d0e6c2
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 17:36:40 -0300

    adiciona classe ItemPedido/ItemPedido.cs

--- ItemPedido.cs
+++ ItemPedido.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace p2pratico
 {
-    internal class ItemPedido
+    public class ItemPedido
+{
+    public Produto Produto { get; }
+    public int Quantidade { get; }
+
+    public ItemPedido(Produto produto, int quantidade)
     {
+        Produto = produto;
+        Quantidade = quantidade;
     }
+
+    public decimal CalcularValorTotal()
+    {
+        return Produto.Preco * Quantidade;
+    }
+}
 }
