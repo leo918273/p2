13b808bb802d361a57ffdc74437d9b4e0e9a2fa3
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 17:44:59 -0300

    estrutura da classe Pedido/Pedido.cs

--- Pedido.cs
+++ Pedido.cs
@@ -1,4 +1,4 @@
-﻿using System;
+.﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace p2pratico
 {
-    internal class Pedido
-    {
-    }
+public int Id { get; }
+public Cliente Cliente { get; }
+public List<ItemPedido> Itens { get; }
+public DateTime Data { get; }
+public decimal Total { get; private set; }
+
+public Pedido(int id, Cliente cliente, List<ItemPedido> itens, DateTime data)
+{
+    Id = id;
+    Cliente = cliente;
+    Itens = itens;
+    Data = data;
+}
+
 }
