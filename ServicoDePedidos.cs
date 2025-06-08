52762a6a35912fbe8bb3aea20a189920a5804b0e
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 20:24:36 -0300

    Update ServicoDePedidos.cs

--- ServicoDePedidos.cs
+++ ServicoDePedidos.cs
@@ -6,17 +6,26 @@ using System.Threading.Tasks;
 
 namespace p2
 {
-  public class ServicoPedido
-  {
-    private readonly RepositorioPedido _repositorio;
-    private readonly ILog _logger;
-    private readonly List<IDescStrategy> _estrategias;
-
-    public ServicoPedido(RepositorioPedido repositorio, ILog logger, List<IDescStrategy> estrategias)
+    public class ServicoPedido
     {
-       _repositorio = repositorio;
-      _logger = logger;
-      _estrategias = estrategias;
+        private readonly RepositorioPedido _repositorio;
+        private readonly ILog _logger;
+        private readonly List<IDescStrategy> _estrategias;
+
+        public ServicoPedido(RepositorioPedido repositorio, ILog logger, List<IDescStrategy> estrategias)
+        {
+            _repositorio = repositorio;
+            _logger = logger;
+            _estrategias = estrategias;
+        }
+
+        public void ProcessarPedido(Pedido pedido)
+        {
+            pedido.CalcularTotal(_estrategias);
+            _repositorio.Salvar(pedido);
+            _logger.Log($"Pedido {pedido.Id} processado para cliente {pedido.Cliente.Nome}. Total: R${pedido.Total:F2}");
+        }
     }
-  }
+
+
 }
