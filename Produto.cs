13e4b10f18cda6975a0ba419226a5970bc7e33e2
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 17:17:18 -0300

    adiciona a classe produto/Produto.cs

--- Produto.cs
+++ Produto.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace p2pratico
 {
-    internal class Produto
-    {
-    }
+ public class Produto
+ {
+     public int Id { get; }
+     public string Nome { get; }
+     public decimal Preco { get; }
+     public string Categoria { get; }
+
+     public Produto(int id, string nome, decimal preco, string categoria)
+     {
+         if (preco <= 0)
+             throw new ArgumentException("O preço do produto tem q ser maior do q zero.");
+
+         Id = id;
+         Nome = nome;
+         Preco = preco;
+         Categoria = categoria;
+     }
+ }
 }
