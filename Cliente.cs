3c752b78d3fa8382bc136d52d38252853a873eea
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 17:27:21 -0300

    adicionou a classe cliente/Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace p2pratico
 {
-    internal class Cliente
+    public class Cliente
+{
+    public int Id { get; }
+    public string Nome { get; }
+    public string Email { get; }
+    public string Cpf { get; }
+
+    public Cliente(int id, string nome, string email, string cpf)
     {
+        Id = id;
+        Nome = nome;
+        Email = email;
+        Cpf = cpf;
     }
+}
 }
