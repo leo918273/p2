fe334de44da562914ba3a629b83e9d131e6d6ebd
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 20:22:34 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+
 using p2;
 using System;
 using System.Collections.Generic;
@@ -6,25 +7,25 @@ public class Program
 {
     public static void Main()
     {
-        var produto1 = new Produto(1, "notebook", 3000m, "Eletrônicos");
+        var produto1 = new Produto(1, "Notebook", 3000m, "Eletrônicos");
         var produto2 = new Produto(2, "Caderno", 20m, "Papelaria");
 
-        var cliente = new Cliente(1, "João", "Joao@gmail.com", "123.456.789-00");
+        var cliente = new Cliente(1, "João Silva", "joao@email.com", "123.456.789-00");
 
         var itensPedido = new List<ItemPedido>
         {
             new ItemPedido(produto1, 1),
-            new ItemPedido(produto2, 3)
+            new ItemPedido(produto2, 6)
         };
 
         var fabricaPedido = new FabricaDePedido();
         var pedido = fabricaPedido.CriarPedido(1, cliente, itensPedido);
 
         var repositorioPedido = new RepositorioPedido();
-        var logger = new ConsoleLog();
-        var estrategiaDesconto = new List<IDescStrategy> { new DescCategoria(), new DescQuantidade() };
+        var logger = new ConsoleLogger();
+        var estrategiasDesconto = new List<IDescStrategy> { new DescCategoria(), new DescontoPorQuantidade() };
 
-        var servicoPedido = new ServicoPedido(repositorioPedido, logger, estrategiaDesconto);
+        var servicoPedido = new ServicoPedido(repositorioPedido, logger, estrategiasDesconto);
         servicoPedido.ProcessarPedido(pedido);
 
         foreach (var p in repositorioPedido.ListarTodos())
