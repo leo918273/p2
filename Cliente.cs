14f66307c7c7b3a3992c03063cd198a5ca508608
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 20:15:26 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,18 +7,19 @@ using System.Threading.Tasks;
 namespace p2
 {
     public class Cliente
-{
-    public int Id { get; }
-    public string Nome { get; }
-    public string Email { get; }
-    public string Cpf { get; }
-
-    public Cliente(int id, string nome, string email, string cpf)
     {
-        Id = id;
-        Nome = nome;
-        Email = email;
-        Cpf = cpf;
+        public int Id { get; }
+        public string Nome { get; }
+        public string Email { get; }
+        public string Cpf { get; }
+
+        public Cliente(int id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
     }
-}
+
 }
