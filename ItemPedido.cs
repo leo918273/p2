8f30360672eb5ff04e4211acebb8322785523e9e
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 20:18:34 -0300

    Update ItemPedido.cs

--- ItemPedido.cs
+++ ItemPedido.cs
@@ -1,25 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace p2pratico
+namespace p2
 {
     public class ItemPedido
-{
-    public Produto Produto { get; }
-    public int Quantidade { get; }
-
-    public ItemPedido(Produto produto, int quantidade)
     {
-        Produto = produto;
-        Quantidade = quantidade;
+        public Produto Produto { get; }
+        public int Quantidade { get; }
+
+        public ItemPedido(Produto produto, int quantidade)
+        {
+            Produto = produto;
+            Quantidade = quantidade;
+        }
+
+        public decimal CalcularValorTotal()
+        {
+            return Produto.Preco * Quantidade;
+        }
     }
 
-    public decimal CalcularValorTotal()
-    {
-        return Produto.Preco * Quantidade;
-    }
-}
 }
