17cebf588c64523d60b79a35bfa089da85c7e084
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 20:20:47 -0300

    Update Produto.cs

--- Produto.cs
+++ Produto.cs
@@ -1,27 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace p2pratico
+namespace p2
 {
- public class Produto
- {
-     public int Id { get; }
-     public string Nome { get; }
-     public decimal Preco { get; }
-     public string Categoria { get; }
+    public class Produto
+    {
+        public int Id { get; }
+        public string Nome { get; }
+        public decimal Preco { get; }
+        public string Categoria { get; }
 
-     public Produto(int id, string nome, decimal preco, string categoria)
-     {
-         if (preco <= 0)
-             throw new ArgumentException("O preço do produto tem q ser maior do q zero.");
+        public Produto(int id, string nome, decimal preco, string categoria)
+        {
+            if (preco <= 0)
+                throw new ArgumentException("O preço do produto deve ser maior que zero.");
 
-         Id = id;
-         Nome = nome;
-         Preco = preco;
-         Categoria = categoria;
-     }
- }
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+        }
+    }
+
 }
