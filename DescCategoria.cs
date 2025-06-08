6f3c9f7d1685bc74bfc98072fa0cccc9edaa22ed
leo918273 <leonardonobrega350@gmail.com>
2025-06-08 18:48:19 -0300

    lógica de desconto/DescCategoria.cs

---
+++ DescCategoria.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2
+{
+  public class DescCategoria : IDescStrategy
+  {
+    public decimal Calcular(ItemPedido item)
+    {
+      if (item.Produto.Categoria == "eletronico")
+        return item.CalcularValorTotal() * 0.1m
+        return 0;
+    }
+  }
+}
